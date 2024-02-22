8b6ac91a016f6018805d22e24da7d09a8087b09a
zabayrer <zabayrer@s.sfusd.edu>
2024-02-22 14:30:59 -0800

    started working on player doing damage to enemy

--- weapons/test_stick.gd
+++ weapons/test_stick.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 @onready var collision_shape_2d = $CollisionShape2D
 var player: Player
+var enemy: Enemy
+
+var str = 5
 
 func _ready():
 	pass
@@ -10,4 +13,8 @@ func _process(delta):
 	pass
 
 func _on_area_entered(area):
-	print("area")
+	enemy_damage(player.player_atk, enemy.enemy_atk)
+
+func enemy_damage(player_atk,enemy_def):
+	player_atk+str-enemy_def+str
+	#actually do the damage here
