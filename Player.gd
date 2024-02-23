c82b049a23d74244ea4de549cf29007c02dee391
dav <usr21p1@gmail.com>
2024-02-23 14:33:40 -0800

    Hardcoded implemented basic attack

--- Player.gd
+++ Player.gd
@@ -14,6 +14,8 @@ var direction = Vector2.UP
 
 @export var weapon_test_stick: PackedScene
 
+var swing_weapon
+
 func get_player_atk():
 	return player_atk
 
@@ -29,15 +31,19 @@ func _process(delta):
 	if Input.is_action_pressed("move_down"):
 		direction = Vector2.DOWN
 		velocity.y += 1
+		rotation_degrees = 180
 	if Input.is_action_pressed("move_up"):
 		direction = Vector2.UP
 		velocity.y += -1
+		rotation_degrees = 0
 	if Input.is_action_pressed("move_left"):
 		direction = Vector2.LEFT
 		velocity.x += -1
+		rotation_degrees = 270
 	if Input.is_action_pressed("move_right"):
 		direction = Vector2.RIGHT
 		velocity.x += 1
+		rotation_degrees = 90
 	
 	#normalize the velocity
 	velocity = velocity.normalized()
@@ -45,32 +51,61 @@ func _process(delta):
 	#move player
 	position += velocity * speed * delta
 	
+	if swing_weapon:
+		swing_weapon.rotation_degrees += 12
+		if swing_weapon.rotation_degrees >= 45:
+			swing_weapon = null
+	
 	if Input.is_action_just_pressed("melee"):
 		melee_attack()
-		print(get_mouse_direction_from_player())
+		var right_down_vec = Vector2(1, 1).normalized()
+		var right_up_vec = Vector2(1, -1).normalized()
+		var mouse_dir = get_mouse_direction_from_player()
+		
+		var theta_a = acos(right_down_vec.dot(mouse_dir) / (right_down_vec.length() * mouse_dir.length()))
+		var theta_b = acos(right_up_vec.dot(mouse_dir) / (right_up_vec.length() * mouse_dir.length()))
+		
+		var swing_direction
+		if theta_a < PI / 2:
+			if theta_b < PI / 2:
+				# Right 
+				swing_direction = Vector2.RIGHT
+			else:
+				# Down
+				swing_direction = Vector2.DOWN
+		else:
+			if theta_b < PI / 2:
+				# Up
+				swing_direction = Vector2.UP
+			else:
+				# Left
+				swing_direction = Vector2.LEFT
+				
+		swing_weapon.rotation_degrees = -45
+				
+		print(swing_direction)
 	
 	if Input.is_action_just_pressed("ranged"):
 		ranged_attack()
 		print("ranged")
 		
 func get_mouse_direction_from_player():
-	var viewport_w = get_viewport().size.x
-	var viewport_h = get_viewport().size.y
+	var mouse_pos = get_viewport().get_mouse_position()
+	var viewport = get_viewport().size
+	var screen_mouse_pos = Vector2(mouse_pos.x / viewport.x, mouse_pos.y / viewport.y)
+	var screen_coord = (screen_mouse_pos - (Vector2(position.x / viewport.x, position.y / viewport.y))) * 2
 	
-	var mouse_global_pos = get_viewport().get_mouse_position()
-	
-	print(viewport_w, " ", viewport_h, " ", mouse_global_pos)
-	
-	return Vector2(mouse_global_pos.x / viewport_w, mouse_global_pos.y / viewport_h)
+	return screen_coord.normalized()
 
 func melee_attack():
-	var test_stick = weapon_test_stick.instantiate()
-	test_stick.player = self
-	add_child(test_stick)
+	if !swing_weapon:
+		var test_stick = weapon_test_stick.instantiate()
+		test_stick.player = self
+		swing_weapon = test_stick
+		add_child(test_stick)
 
 func ranged_attack():
 	pass
-
 
 #die
 func on_death():
