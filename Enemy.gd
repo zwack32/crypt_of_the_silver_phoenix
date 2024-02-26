29685c0157e108075c7e51aa31dcaa7cecb8a750
zabayrer <zabayrer@s.sfusd.edu>
2024-02-25 17:53:49 -0800

    trying to move enemy damage taking to enemy script
    it's not working lol, i need to get a reference to the weapon
    
    i made the stick layer 1 cuz i don't even know

--- Enemy.gd
+++ Enemy.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name Enemy
 
 @export var player: Area2D
+@export var stick: Area2D
 @export var speed: float = 2.0
 
 var enemy_max_health = 20
@@ -19,8 +20,9 @@ func get_enemy_max_hp():
 	return enemy_max_health
 func get_enemy_health():
 	return enemy_health
-	
-	
+#this worked for the player; idk what else to do
+#test_stick.enemy = self
+
 func _ready():
 	pass 
 
@@ -31,4 +33,15 @@ func _process(delta):
 
 #Hit player
 func _on_area_entered(area):
-	player.deal_damage(enemy_atk)
+	if area == player:
+		player.deal_damage(enemy_atk)
+		print("player")
+	else: # area == weapon:
+	#	enemy_take_damage(player.get_player_atk(), enemy_def, enemy_health, sword_str)
+		print("weapon")
+
+func enemy_take_damage(player_atk,enemy_def,enemy_hp, sword_str):
+	var new_enemy_health = enemy_hp
+	new_enemy_health -= (clamp(player_atk+sword_str-enemy_def, 0, 9999999))+sword_str
+	enemy_hp = new_enemy_health
+	
