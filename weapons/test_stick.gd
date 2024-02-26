7f0847f1f36162b53193b2893208be3dd9cceb6f
zabayrer <zabayrer@s.sfusd.edu>
2024-02-25 18:00:28 -0800

    adjusted layers. this makes no sense.

--- weapons/test_stick.gd
+++ weapons/test_stick.gd
@@ -14,9 +14,10 @@ func _process(delta):
 
 func _on_area_entered(area):
 	#enemy_damage(player.get_player_atk(), enemy.get_enemy_def(), enemy.get_enemy_health())
-	print(player.get_player_atk())
+	#print(player.get_player_atk())
 	#print(enemy.get_enemy_def())
 	#print(enemy.get_enemy_health())
+	pass
 
 func enemy_damage(player_atk,enemy_def,enemy_hp):
 	var new_enemy_health = enemy_hp
