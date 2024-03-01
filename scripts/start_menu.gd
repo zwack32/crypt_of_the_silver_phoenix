f0faf9c855f9c73d2fafe884f472abbf15321fde
MAZHI3 <122053601+MAZHI3@users.noreply.github.com>
2024-03-01 14:31:30 -0800

    level transition

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,15 +2,16 @@ extends CanvasLayer
 
 @onready var start_button = $CenterContainer/VBoxContainer/StartButton
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_button.grab_focus()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+func _on_start_button_pressed():
+	await LevelTransition.fade_to_black()
+	LevelTransition.fade_from_black()
+	
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
