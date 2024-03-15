9a7a85947f0a3f6bc0d4ab840a21a6a74a0f6efe
zabayrer <zabayrer@s.sfusd.edu>
2024-03-14 21:03:50 -0700

    theoretically fixed sunburst

--- tomes/sunburst.gd
+++ tomes/sunburst.gd
@@ -5,19 +5,10 @@ var str = 0
 var velocity = Vector2.ZERO
 var speed = 700.0
 
-@onready var bead_collison_shape = $BeadCollisionShape
-@onready var bead_sprite = $BeadSprite
-@onready var blast_collision_shape = $BlastCollisionShape
-@onready var blast_sprite = $BlastSprite
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bead_collison_shape.enable()
-	bead_sprite.enable()
 	
-	blast_collision_shape.disable()
-	blast_sprite.disable()
 	
 	var mouse_pos = get_viewport().get_mouse_position()
 	
@@ -33,11 +24,7 @@ func _process(delta):
 func _on_area_entered(area):
 	str = 5
 	
-	bead_collison_shape.disable()
-	bead_sprite.disable()
-	
-	blast_collision_shape.enable()
-	blast_sprite.enable()
+	scale *= 3
 	
 	await get_tree().create_timer(0.016).timeout
 	set_collision_layer_value(1, 0)
