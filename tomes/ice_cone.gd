2deb249c6544b40066afbe53d3ebfc646cba9278
dav <usr21p1@gmail.com>
2024-03-18 20:15:46 -0700

    Add winter sneeze

--- tomes/ice_cone.gd
+++ tomes/ice_cone.gd
@@ -4,6 +4,8 @@ var velocity = Vector2.ZERO
 var str = 7
 var type = "ice"
 
+@onready var cpu_particles_2d = $CPUParticles2D
+
 func _ready():
 	var mouse_pos = get_viewport().get_mouse_position()
 	var viewport = get_viewport().size
@@ -11,12 +13,9 @@ func _ready():
 	var coord = screen_mouse_pos * 2.0 - Vector2(1.0, 1.0)
 	rotation = coord.angle() + PI / 2
 	position = player.position
-
-func _process(delta):
-	await get_tree().create_timer(delta).timeout
-	set_collision_layer_value(1, 0)
+	cpu_particles_2d.emitting = true
 
 func _on_area_entered(area):
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.5).timeout
 	queue_free()
 	
