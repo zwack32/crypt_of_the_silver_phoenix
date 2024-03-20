16c5f55dabda2367f5f04a0761a0a185d313a06b
zabayrer <zabayrer@s.sfusd.edu>
2024-03-19 17:09:53 -0700

    made button only pressed once

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,16 +1,18 @@
 extends CanvasLayer
 
 @onready var start_button = $CenterContainer/VBoxContainer/StartButton
-
+var pressed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_button.grab_focus()
 
 func _on_start_button_pressed():
-	await LevelTransition.fade_to_black(1)
-	get_tree().change_scene_to_file("res://scenes/test_world.tscn")
-	LevelTransition.fade_from_black(1)
+	if !pressed:
+		pressed = true
+		await LevelTransition.fade_to_black(1)
+		get_tree().change_scene_to_file("res://scenes/test_world.tscn")
+		LevelTransition.fade_from_black(1)
 	
 
 func _on_quit_button_pressed():
