abe3d0b65ac6e62a4a9b96200f7361589f0cae1a
dav <usr21p1@gmail.com>
2024-03-20 12:00:48 -0700

    Add level text

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -2,6 +2,10 @@ extends CanvasLayer
 
 @onready var weapon_rect = $WeaponRect
 @onready var tome_rect = $TomeRect
+@onready var level_label = $RichTextLabel
+
+func _ready():
+	level_label.text = "Level: " + str(Progression.get_dungeon_level())
 
 func _process(delta):
 	weapon_rect.texture = WeaponManager.get_melee_texture(WeaponManager.current_melee)
--- scripts/progression.gd
+++ scripts/progression.gd
@@ -3,6 +3,9 @@ extends Node2D
 var dungeon_level = 1
 var initial_enemy_count = 2
 
+func get_dungeon_level() -> int:
+	return dungeon_level
+
 func get_room_count() -> int:
 	if dungeon_level == 1:
 		return 1
