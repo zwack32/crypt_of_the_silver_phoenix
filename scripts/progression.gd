2a43b4f221151864335d1238e08c377f163b5e81
dav <usr21p1@gmail.com>
2024-03-20 11:57:45 -0700

    Add tutorial room

--- scripts/progression.gd
+++ scripts/progression.gd
@@ -4,7 +4,10 @@ var dungeon_level = 1
 var initial_enemy_count = 2
 
 func get_room_count() -> int:
-	return dungeon_level + 6
+	if dungeon_level == 1:
+		return 1
+	else:
+		return dungeon_level + 10
 	
 func get_initial_enemy_count():
 	return initial_enemy_count
