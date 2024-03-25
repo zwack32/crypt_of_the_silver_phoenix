6bc271f2f1823345bf3b1a9e3fa3497511821a83
zabayrer <zabayrer@s.sfusd.edu>
2024-03-25 13:43:55 -0700

    working on crosshair

---
+++ scripts/crosshair.gd
@@ -0,0 +1,8 @@
+extends Node2D
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var player = $"../Player"
+
+func _process(delta):
+	position = get_viewport().get_screen_transform() * get_global_transform_with_canvas() * player.local_position
+		#position = get_viewport().get_mouse_position()+player.position
+	#position -= get_viewport().size/2.0
