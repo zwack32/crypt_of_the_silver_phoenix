bd98beed7b4004f31006d9a3156e8c7ac6d08dab
zabayrer <zabayrer@s.sfusd.edu>
2024-03-26 18:45:47 -0700

    drops don't repeat

--- scripts/DropItem.gd
+++ scripts/DropItem.gd
@@ -30,10 +30,9 @@ class DropItemUnion:
 
 var is_in_range = false
 var drop_item: DropItemUnion = DropItemUnion.new()
-
+var data
 func _ready():
 	var drop_type = randi_range(0, DropItemType._Count - 1)
-	var data
 	if drop_type == DropItemType.Health:
 		data = randi_range(0, DropHeathValues._Count - 1)
 		if data == DropHeathValues.HealthSmall:
@@ -43,11 +42,9 @@ func _ready():
 		if data == DropHeathValues.HealthBig:
 			sprite_2d.texture = load("res://art/large_heart.png")
 	elif drop_type == DropItemType.Melee:
-		data = randi_range(0, MeleeWeaponType._Count - 1)
-		sprite_2d.texture = WeaponManager.get_melee_texture(data)
+		drop_melee()
 	elif drop_type == DropItemType.Tome:
-		data = randi_range(0, TomeType._Count - 1)
-		sprite_2d.texture = WeaponManager.get_tome_texture(data)
+		drop_tome()
 		
 	drop_item.ty = drop_type
 	drop_item.data = data
@@ -78,3 +75,17 @@ func _on_area_exited(area):
 	if area.owner is Player:
 		is_in_range = false
 		sprite_2d.modulate.a = 1.0
+
+func drop_melee():
+	data = randi_range(0, MeleeWeaponType._Count - 1)
+	if data != WeaponManager.current_melee:
+		sprite_2d.texture = WeaponManager.get_melee_texture(data)
+	else:
+		drop_melee()
+
+func drop_tome():
+	data = randi_range(0, TomeType._Count - 1)
+	if data != WeaponManager.current_tome:
+		sprite_2d.texture = WeaponManager.get_tome_texture(data)
+	else:
+		drop_tome()
