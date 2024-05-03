d314ad849a461841c9fa5e9edc2e401865b7768f
zabayrer <zabayrer@s.sfusd.edu>
2024-05-03 14:32:29 -0700

    actually did the landmine thing oops

--- tomes/landmine.gd
+++ tomes/landmine.gd
@@ -1,11 +1,29 @@
-extends Area2D
+#this is mostly copy-pasted from a broken sunburstl; fix later
 
+extends Tome
+var str = 15
+var velocity = Vector2.ZERO
+var speed = 70.0
+var type = "glow"
+var entered_once = false
+@export var cooldown = 4.0
+@onready var bead_sprite = $BeadSprite
+@onready var cpu_particles_2d = $CPUParticles2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	look_at(get_global_mouse_position())
+	position = player.position
+	
 func _process(delta):
-	pass
+	position += velocity
+	set_collision_layer_value(1, 0)
+	
+func _on_area_entered(area):
+	bead_sprite.hide()
+	if !entered_once:
+		entered_once = true
+	scale = Vector2(4, 4)
+	cpu_particles_2d.emitting = true
+	velocity = Vector2.ZERO
+	await get_tree().create_timer(0.3).timeout
+	queue_free()
