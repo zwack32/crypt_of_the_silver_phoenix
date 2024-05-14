37351b336542e77f8e02cbc26c7732a3930d3f4b
zabayrer <zabayrer@s.sfusd.edu>
2024-05-14 12:24:03 -0700

    added in background music

--- scripts/test_world.gd
+++ scripts/test_world.gd
@@ -2,13 +2,8 @@ extends Node2D
 
 @export var is_tutorial = false
 
-#@onready var bgm = $BGM
+@onready var standard_bgm = $standard_theme_bgm
 @onready var tutorial_nodes = $TutorialNodes
 
 func _ready():
-	#bgm.play()
-	
-	if is_tutorial:
-		pass
-		# TODO work on tutorial UI
-		# tutorial_nodes.show()
+	standard_bgm.play()
