f0c2e648aee5bc3f287369367ffd1249054a13f6
zabayrer <zabayrer@s.sfusd.edu>
2024-05-16 14:29:13 -0700

    added fade out on death music

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,15 +1,21 @@
 extends CanvasLayer
 @onready var restart_button = $RestartButton
 var twice = false
-
+@onready var death_theme = $DeathTheme
+var fade_out = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	music_ready()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if fade_out and death_theme.volume_db >= -200:
+		death_theme.volume_db -= 0.05
+		print(death_theme.volume_db)
+		#await get_tree().create_timer(0.1).timeout
 	pass
+	
 
 func _on_restart_button_pressed():
 	if twice:
@@ -20,3 +26,7 @@ func _on_restart_button_pressed():
 	await LevelTransition.fade_to_black(1)
 	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
 	await LevelTransition.fade_from_black(1)
+	
+func music_ready():
+	await get_tree().create_timer(4).timeout
+	fade_out = true
