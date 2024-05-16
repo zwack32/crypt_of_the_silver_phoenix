cc9136cc3d27dd8d7e46c4bd9c31f2df0b814506
dav <usr21p1@gmail.com>
2024-05-16 12:22:47 -0700

    Allow mage spell to hit wall

--- scripts/MageSpell.gd
+++ scripts/MageSpell.gd
@@ -15,29 +15,34 @@ var spell_atk = 10
 #@onready var cpu_particles_2d = $CPUParticles2D
 @onready var spell_animation = $"Spell Animation"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = mage.position
 	velocity = (player.position - position) * speed
 	bead_sprite.show()
 	spell_animation.hide()
+	
+	await get_tree().create_timer(0.3).timeout
 
 func _process(delta):
 	position += velocity * delta
 	set_collision_layer_value(1, 0)
 
 func _on_area_entered(area):
-	bead_sprite.hide()
-	spell_animation.show()
-	player.take_damage(spell_atk)
 	if !entered_once && area.owner is Player: 
+		player.take_damage(spell_atk)
 		entered_once = true
 		scale = Vector2(4, 4)
-		#cpu_particles_2d.emitting = true
-		spell_animation.play("mage_spell")
-		velocity = Vector2.ZERO
-		
-		await get_tree().create_timer(0.3).timeout
-		queue_free()
+		boom()
 
+func _on_body_entered(body):
+	boom()
+
+func boom():
+	bead_sprite.hide()
+	spell_animation.show()
+	spell_animation.play("mage_spell")
+	velocity = Vector2.ZERO
+		
+	await get_tree().create_timer(0.3).timeout
+	queue_free()
