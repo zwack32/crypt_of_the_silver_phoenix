0b8eb10d0bbfb4cb2757ff2a772b895b2c1b001a
zabayrer <zabayrer@s.sfusd.edu>
2024-05-17 13:43:11 -0700

    fade in on start of level

--- scripts/test_world.gd
+++ scripts/test_world.gd
@@ -6,4 +6,12 @@ extends Node2D
 @onready var tutorial_nodes = $TutorialNodes
 
 func _ready():
-	standard_bgm.play()
+	standard_bgm.volume_db = -50
+	pass
+
+func _process(delta):
+	if standard_bgm.volume_db <= 0:
+		standard_bgm.volume_db += 0.5
+		print(standard_bgm.volume_db)
+		#await get_tree().create_timer(0.1).timeout
+	pass
