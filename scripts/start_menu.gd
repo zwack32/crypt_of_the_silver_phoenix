bb93b1fa5a3cdce0c10330c4db9ff8f8d26ae32f
zabayrer <zabayrer@s.sfusd.edu>
2024-05-17 13:27:17 -0700

    added title theme to start, working on fade out

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,10 +2,10 @@ extends CanvasLayer
 
 @onready var start_button = $CenterContainer/HBoxContainer/StartButton
 @onready var animated_title = $AnimatedTitle
-
+@onready var title_theme = $TitleTheme
 
 var pressed = false
-
+var music_fade = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animated_title.play()
@@ -15,6 +15,7 @@ func _ready():
 
 func _on_start_button_pressed():
 	if !pressed:
+		music_fade = true
 		pressed = true
 		await LevelTransition.fade_to_black(1)
 		get_tree().change_scene_to_file("res://scenes/test_world.tscn")
@@ -25,6 +26,10 @@ func _on_start_button_pressed():
 func _on_quit_button_pressed():
 	get_tree().quit()
 
+#func _process(delta):
+#	if music_fade:
+#		title_theme.volume_db -= 0.1
+#		print(title_theme.vo
 
 
 func _on_tutorial_pressed():
