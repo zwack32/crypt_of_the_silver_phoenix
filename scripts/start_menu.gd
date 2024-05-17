a1def39db4b7b338568dc812340d50dfa69f963e
zabayrer <zabayrer@s.sfusd.edu>
2024-05-17 13:32:03 -0700

    fade out when button

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -26,10 +26,10 @@ func _on_start_button_pressed():
 func _on_quit_button_pressed():
 	get_tree().quit()
 
-#func _process(delta):
-#	if music_fade:
-#		title_theme.volume_db -= 0.1
-#		print(title_theme.vo
+func _process(delta):
+	if music_fade:
+		title_theme.volume_db -= 0.5
+		print(title_theme.volume_db)
 
 
 func _on_tutorial_pressed():
