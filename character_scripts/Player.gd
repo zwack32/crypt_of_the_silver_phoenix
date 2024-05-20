f0d897ca7f82f3d92834bc723ad85b9abc87bda0
dav <usr21p1@gmail.com>
2024-05-20 15:47:16 -0700

    HUGE

--- character_scripts/Player.gd
+++ character_scripts/Player.gd
@@ -138,22 +138,30 @@ func move():
 	if Input.is_action_pressed("move_down"):
 		direction = Vector2.DOWN
 		player_velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction = Vector2.UP
+		player_velocity.y += -1
+	if Input.is_action_pressed("move_left"):
+		direction = Vector2.LEFT
+		player_velocity.x += -1
+	if Input.is_action_pressed("move_right"):
+		direction = Vector2.RIGHT
+		player_velocity.x += 1
+		
+	if Input.is_action_pressed("move_left"):
+		animated_sprite_2d.play("walk_left")
+	elif Input.is_action_pressed("move_right"):
+		animated_sprite_2d.play("walk_right")
+	elif Input.is_action_pressed("move_down"):
 		if !Input.is_action_pressed("move_left") or !Input.is_action_pressed("move_right"):
 			animated_sprite_2d.play("walk_down")
-	if Input.is_action_pressed("move_up"):
+	elif Input.is_action_pressed("move_up"):
 		direction = Vector2.UP
 		player_velocity.y += -1
 		if !Input.is_action_pressed("move_left") or !Input.is_action_pressed("move_right"):
 			animated_sprite_2d.play("walk_up")
-	if Input.is_action_pressed("move_left"):
-		direction = Vector2.LEFT
-		player_velocity.x += -1
-		animated_sprite_2d.play("walk_left")
-	if Input.is_action_pressed("move_right"):
-		direction = Vector2.RIGHT
-		player_velocity.x += 1
-		animated_sprite_2d.play("walk_right")
-	elif Input.is_action_just_released("move_down"):
+		
+	if Input.is_action_just_released("move_down"):
 		animated_sprite_2d.stop()
 		# animated_sprite_2d.play("idle_transition")
 		# await get_tree().create_timer(1).timeout
@@ -173,6 +181,7 @@ func move():
 		# animated_sprite_2d.play("idle_transition")
 		# await get_tree().create_timer(1).timeout
 		animated_sprite_2d.play("idle")
+		
 	#remove this before releasing the game
 	if Input.is_action_just_pressed("reroll"):
 		WeaponManager.reset()
